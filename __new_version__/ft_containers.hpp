102d6b95c02b62ebf7fe5b8e5cca2c626e6daf1d
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-01-30 04:09:42 +0100

    shared header

--- __new_version__/ft_containers.hpp
+++ __new_version__/ft_containers.hpp
@@ -2,7 +2,7 @@
 /**	@file ft_containers.hpp
  * 
  * 	This file contain the general shared element used 
- * 	in this reimplementation of STL containers
+ * 	in this reimplementation of STL containers.
  */
 
 # pragma once
@@ -11,3 +11,23 @@
 
 # define FT_NAMESPACE ft
 
+namespace FT_NAMESPACE
+{
+
+	/**
+	 * 	@brief Check type
+	 * 
+	 * 	Compares 2 value types.
+	 * 
+	 * 	@tparam Target: the target type.
+	 * 	@tparam Required: the required type.
+	 * 	@return If Target == Required return true.
+	*/
+	template <typename Target, typename Required>
+	inline bool check_type()
+	{
+		typename iterator_traits<Target>::iterator_category* t;
+
+		return (dynamic_cast<Required*>(t));
+	}
+};
