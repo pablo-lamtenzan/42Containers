951ca4d871dea510fe392ecae8296f6dcf1c6110
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-02-18 10:55:49 +0100

    automation

--- tester.sh
+++ tester.sh
@@ -4,7 +4,7 @@ TESTER_LOG_DIR=__TESTER__
 MAIN=main.cpp
 COMPILER=clang++
 SAFE_FLAGS=
-CHECK_CPP_VERSION=
+CHECK_CPP_VERSION= # remove auto and stop using it is c++11
 
 set -e
 
@@ -12,7 +12,7 @@ rm -rf ${TESTER_LOG_DIR}
 mkdir ${TESTER_LOG_DIR}
 ${COMPILER} ${SAFE_FLAGS} ${CHECK_CPP_VERSION} "${TESTER_IMPL_DIR}/${MAIN}"
 ./a.out ; rm ./a.out
-read -p "Do you you want to delete the test log ? [y/n] " -n1 -r ANSWER
+read -p "Do you want to delete the test log ? [y/n] " -n1 -r ANSWER
 echo
 if [[ ${ANSWER} =~ ^[Yy]$ ]]; then
 	echo "The test log has been cuccesfully deleted !"
