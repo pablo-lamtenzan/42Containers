fbc3fe5e2c36c014834a1851aa483c9984f90fe4
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-02-18 10:57:30 +0100

    rm random access tests (imcompatible with list)

--- tests/shared_test_linear.hpp
+++ tests/shared_test_linear.hpp
@@ -19,8 +19,6 @@
 
 // NOTE: Move random access test for list
 
-# define RANDOM_ACCESS 0
-
 /**
  * 	@brief cond == true if the template type is random_access
 */
@@ -121,88 +119,6 @@ inline static void test_assignation(std::ofstream& fd)
 		fd << *i << std::string(" ");
 	fd << "}" << std::endl;
 }
-
-#if RANDOM_ACCESS
-
-template <typename Container, typename T>
-inline static void test_random_access(std::ofstream& fd)
-{
-	fd << std::endl << "-------------- RANDOM ACCESS ----------------" << std::endl;
-
-	// TEST: at
-
-	Container c(size_t(42));
-
-	for (size_t i = 0 ; i < 42 ; i++)
-		c.at(i) = T(i);
-	fd << "\'At\' test 1: " << std::endl << "{ ";
-	for (size_t i = 0 ; i < 42 ; i++)
-	{
-		c.at(i) = c.at(i) << T(1 & i);
-		fd << c.at(i) << static_cast<std::string>(i == 41 ? "" : " ");
-	}
-	fd << " }" << std::endl;
-
-	fd << "\'At\' test 2: ";
-	try {
-		c.at(42) = T();
-		fd << "ERROR" << std::endl;
-	} catch (...) { fd << "SUCCESS" << std::endl; }
-	
-
-	// TEST: operator[]
-
-	c.clear();
-
-	for (size_t i = 0 ; i < 42 ; i++)
-		c[i] = T(i);
-	fd << "\'operator[]\' test 1: " << std::endl << "{ ";
-	for (size_t i = 0 ; i < 42 ; i++)
-	{
-		c[i] = c[i] << T(1 & i);
-		fd << c[i] << static_cast<std::string>(i == 41 ? "" : " ");
-	}
-	fd << " }" << std::endl;
-
-	fd << "\'operator[]\' test 2: ";
-	pid_t pid = fork();
-	if (pid == 0)
-	{
-		c[0xffff] = T();
-		exit(EXIT_FAILURE); // Will never execute this line.
-	}
-	else if (pid < 0)
-		std::cerr << "Syscall fork failed for misterious reasons ..."
-		<< "[Check the memory avalaible in your device]" << std::endl;
-
-	int wstatus;
-	while (waitpid(pid, &wstatus, 0) >= 0);
-	if (WIFSIGNALED(wstatus) && WTERMSIG(wstatus) == SIGSEGV)
-		fd << "ALL OK" << std::endl;
-	else
-		fd << "ERROR" << std::endl;
-
-	// NOTE: Some containers wont segfault if the operator at overflow with this test.
-	fd << "\'operator[]\' test 3: ";
-	pid_t pid2 = fork();
-	if (pid2 == 0)
-	{
-		c[42] = T();
-		exit(EXIT_FAILURE); // Will never execute this line.
-	}
-	else if (pid2 < 0)
-		std::cerr << "Syscall \'fork\' failed for misterious reasons ..."
-		<< "[Check the memory avalaible in your device]" << std::endl;
-
-	int wstatus2;
-	while (waitpid(pid2, &wstatus2, 0) >= 0);
-	if (WIFSIGNALED(wstatus2) && WTERMSIG(wstatus2) == SIGSEGV)
-		fd << "SEGFAULT" << std::endl;
-	else
-		fd << "DON'T SEGFAULT" << std::endl;
-}
-
-#endif
 
 template <typename Container, typename T>
 inline static void test_element_access(std::ofstream& fd)
@@ -445,6 +361,7 @@ inline static void test_modifiers(std::ofstream& fd)
 	for (auto i = ii.begin() ; i != ii.end() ; i++)
 		fd << *i << " ";
 	fd << " }" << std::endl;
+
 }
 
 template <typename Container, typename T>
@@ -545,14 +462,6 @@ inline static void shared_tests(const std::string& filename)
 
 	for (size_t i = 0 ; i < sizeof(tests) / sizeof(*tests) ; i++)
 		tests[i](fd);
-
-#if RANDOM_ACCESS
-
-	if (are_same<typename Container::iterator::iterator_category,
-	std::random_access_iterator_tag>::cond)
-		test_random_access<Container, T>(fd);
-
-#endif
 	
 	fd.close();
 }
@@ -563,4 +472,7 @@ inline void execute_shared_tests_for_linear_containers()
 	shared_tests<std::vector<T>, T>(std::string(std::string(TEST_DIR) + std::string(STD_VECTOR_FILENAME_SHARED)));
 	shared_tests<std::list<T>, T>(std::string(std::string(TEST_DIR) + std::string(STD_LIST_FILENAME_SHARED)));
 	shared_tests<std::deque<T>, T>(std::string(std::string(TEST_DIR) + std::string(STD_DEQUE_FILENAME_SHARED)));
+	shared_tests<std::vector<T>, T>(std::string(std::string(TEST_DIR) + std::string(FT_VECTOR_FILENAME_SHARED)));
+	shared_tests<std::list<T>, T>(std::string(std::string(TEST_DIR) + std::string(FT_LIST_FILENAME_SHARED)));
+	shared_tests<std::deque<T>, T>(std::string(std::string(TEST_DIR) + std::string(FT_DEQUE_FILENAME_SHARED)));
 }
