79bdff643d6c23149874f7f3846c70a70f9ee822
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-02-19 08:49:02 +0100

    deque targeted tests

--- tests/deque_tests.hpp
+++ tests/deque_tests.hpp
@@ -1,7 +1,37 @@
 
 # pragma once
 
-inline void deque_tests()
-{
+# include "tester.hpp"
+# include "vector_tests.hpp"
 
+# include <deque>
+
+template <typename Container, typename T>
+inline static void test_deque_resize(std::ofstream& fd)
+{
+	fd << std::endl << "-------------- RESIZE ----------------" << std::endl;
+
+	// TO DO:
+
+	fd << "TO DO" << std::endl;
+}
+
+template <typename Container, typename T>
+inline static void start_deque_tests(const std::string& filename)
+{
+	static void (*const tests[])(std::ofstream&) = {
+		&test_random_access<Container, T>,
+		&test_push_pop_back<Container, T>,
+		&test_push_pop_front<Container, T>,
+		&test_deque_resize<Container, T>,
+		&test_non_member_iterators<Container, T>,
+	};
+
+	start_tests(filename, tests, ARRAY_SIZE(tests));
+}
+
+inline static void deque_tests()
+{
+	start_deque_tests<std::deque<int>, int>(std::string(std::string(std::string(TEST_DIR) + std::string(STD_DEQUE_FILENAME))));
+	start_deque_tests<std::deque<int>, int>(std::string(std::string(std::string(TEST_DIR) + std::string(FT_DEQUE_FILENAME))));
 }
