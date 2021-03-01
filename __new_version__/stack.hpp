ba4d71413ebf785bc521484cde29f702a9fd0dd4
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2021-03-01 18:11:23 +0100

    new syntax stack class

--- __new_version__/stack.hpp
+++ __new_version__/stack.hpp
@@ -17,11 +17,11 @@ namespace FT_NAMESPACE
 	 * 	@tparam Container the type of value holded by the stack
 	*/
 	template <class T, class Container> // to do set queue by dft
-	class stack
+	struct stack
 	{
 		// TO DO: Probally i have to add the operator== and operator< here too
 
-		public:
+		/* Member types */
 
 		typedef Container									container_type;
 		typedef typename container_type::value_type			value_type;
@@ -29,80 +29,114 @@ namespace FT_NAMESPACE
 		typedef typename container_type::const_reference	const_reference;
 		typedef typename container_type::size_type			size_type;
 
-		/* Esp container */
+		/* Member fucntions */
+
+		explicit stack(const_reference other = container_type());
+
+		// TO DO: operator= & swap
+	
+		/* Element access */
+		reference 		top();
+		const_reference top() const;
+
+		/* Capacity */
+		bool			empty() const;
+		size_type		size() const;
+
+		/* Modifiers */
+		void			push(const_reference value);
+		void			pop();
+
+		/* Core */
 
 		protected:
 
-		Container			c;
-
-		public:
-
-		/**
-		 * 	@brief Default Constructor
-		*/
-		explicit stack(const_reference other = container_type()) : c(other) { }
-
-		/**
-		 * 	@brief empty
-		 * 
-		 * 	@return True if %stack is empty.
-		*/
-		bool		empty() const
-		{
-			return (c.empty());
-		}
-
-		/**
-		 * 	@brief size
-		 * 
-		 * 	@return The amount of elements holded by %stack
-		*/
-		size_type	size() const
-		{
-			return (c.size());
-		}
-
-		/**
-		 * 	@brief top
-		 * 
-		 * 	@return A reference to the first element in %stack.
-		*/
-		reference 	top()
-		{
-			return (c.back());
-		}
-
-		/**
-		 * 	@brief top
-		 * 
-		 * 	@return A const reference to the first element in %stack.
-		*/
-		const_reference top() const
-		{
-			return (c.back());
-		}
-
-		/**
-		 * 	@brief push
-		 * 
-		 * 	Prappend one element in the top of %stack.
-		*/
-		void		push(const_reference value)
-		{
-			c.push_back(value);
-		}
-
-
-		/**
-		 * 	@brief pop
-		 * 
-		 * 	Removes the element located at the top of %stack.
-		*/
-		void		pop()
-		{
-			c.pop_back();
-		}
+		container_type	c;
 	};
+
+	/**
+	 * 	@brief Default Constructor
+	*/
+	template <class T, class Container>
+	stack<T, Container>::stack(const_reference other = container_type())
+	: c(other)
+	{ }
+
+	////////////////////
+	// Element access //
+	////////////////////
+
+	/**
+	 * 	@brief top
+	 * 
+	 * 	@return A reference to the first element in %stack.
+	*/
+	template <class T, class Container>
+	typename stack<T, Container>::reference
+	stack<T, Container>::top()
+	{ return (c.back()); }
+
+	/**
+	 * 	@brief top
+	 * 
+	 * 	@return A const reference to the first element in %stack.
+	*/
+	template <class T, class Container>
+	typename stack<T, Container>::const_reference
+	stack<T, Container>::top() const
+	{ return (c.back()); }
+
+	//////////////
+	// Capacity //
+	//////////////
+
+	/**
+	 * 	@brief empty
+	 * 
+	 * 	@return True if %stack is empty.
+	*/
+	template <class T, class Container>
+	bool
+	stack<T, Container>::empty() const
+	{ return (c.empty()); }
+
+	/**
+	 * 	@brief size
+	 * 
+	 * 	@return The amount of elements holded by %stack
+	*/
+	template <class T, class Container>
+	typename stack<T, Container>::size_type
+	stack<T, Container>::size() const
+	{ return (c.size()); }
+
+	//////////////
+	// Modifier //
+	//////////////
+
+	/**
+	 * 	@brief push
+	 * 
+	 * 	Preppend one element in the top of %stack.
+	*/
+	template <class T, class Container>
+	void
+	stack<T, Container>::push(const_reference value)
+	{ c.push_back(value); }
+
+	/**
+	 * 	@brief pop
+	 * 
+	 * 	Removes the element located at the top of %stack.
+	*/
+	template <class T, class Container>
+	void
+	stack<T, Container>::pop()
+	{ c.pop_back(); }
+
+	/////////////////
+	// Non members //
+	/////////////////
 
 	/**
 	 * 	@brief Stack boolen operators
@@ -112,38 +146,32 @@ namespace FT_NAMESPACE
 	 * 	@return The result of the requested boolean operation.
 	*/
 	template <class T, class Container>
-	inline bool 	operator==(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
-	{
-		return (lsh.c == rhs.c);
-	}
+	inline bool
+	operator==(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{ return (lsh.c == rhs.c); }
 
 	template <class T, class Container>
-	inline bool 	operator!=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
-	{
-		return (lsh.c != rhs.c);
-	}
+	inline bool
+	operator!=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{ return (lsh.c != rhs.c); }
 
 	template <class T, class Container>
-	inline bool 	operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
-	{
-		return (lsh.c < rhs.c);
-	}
+	inline bool
+	operator<(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{ return (lsh.c < rhs.c); }
 
 	template <class T, class Container>
-	inline bool 	operator<=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
-	{
-		return (lsh.c <= rhs.c);
-	}
+	inline bool
+	operator<=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{ return (lsh.c <= rhs.c); }
 
 	template <class T, class Container>
-	inline bool 	operator>(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
-	{
-		return (lsh.c > rhs.c);
-	}
+	inline bool
+	operator>(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{ return (lsh.c > rhs.c); }
 
 	template <class T, class Container>
-	inline bool 	operator>=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
-	{
-		return (lsh.c >= rhs.c);
-	}
+	inline bool
+	operator>=(const stack<T, Container>& lhs, const stack<T, Container>& rhs)
+	{ return (lsh.c >= rhs.c); }
 };
